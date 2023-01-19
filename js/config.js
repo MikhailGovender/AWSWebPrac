686b646be8e5bc05902d9a8e17a9e26fe2b7229d
Mikhail Govender <mikhailg@bbd.co.za>
2023-01-19 13:43:16 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_JHVmnLH7t', // e.g. us-east-1_JHVmnLH7t
+        userPoolClientId: '30d91v8bfu3j0rt540t7olqna0', // e.g. 30d91v8bfu3j0rt540t7olqna0
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
